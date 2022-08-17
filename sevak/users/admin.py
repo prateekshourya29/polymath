0cd9dd92e9f079b28b9d8f4fde837a193c13e246
Prateek Shourya <prateekshourya29@gmail.com>
2022-08-17 15:50:54 +0530

    Remove registration form in backend.

--- sevak/users/admin.py
+++ sevak/users/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 
-class UserAdmin(UserAdmin):    
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+class UserAdmin(UserAdmin):
     model = User
     list_display = ['email', 'mobile_number']
 
--- sevak/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import User
-
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'last_login', 'date_joined', 'is_staff', 'first_name', 'last_name', 'mobile_number')
